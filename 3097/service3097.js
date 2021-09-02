20ff2f416dbb3c2b83e7961623fa9a9dc1fdefdc
khomyakova_l <khomyakova_l@lwo.by>
2021-09-02 10:50:03 +0300

    update 3097

--- 3097/service3097.js
+++ 3097/service3097.js
@@ -3,31 +3,38 @@ const { query, validationResult } = require('express-validator');
 
 const webServer = express();
 const port = 8180;
+const errorMsg = '<p for="" style="color:red">Validation error!!</p>';
+
+const initForm = (login, password) => {
+ return `<form method="GET" action="http://178.172.195.18:8180/service3097">
+ <label for="">Login</label>
+ <input type="text" name="login" value="${login || ''}">  
+ <label for="">Password</label>
+ <input type="password" name="password" value="${password || ''}">  
+ <input type="submit" value="SEND">
+ </form>`
+};
+
+webServer.use((req, res, next) => {
+  console.log(`* service3097 called, req.originalUrl=${req.originalUrl}`);
+  next();
+});
 
 webServer.get('/service3097',
-  query('login').notEmpty(),
-  query('password').notEmpty().isLength({min: 4}),
+  query('login').optional().notEmpty(),
+  query('password').optional().notEmpty().isLength({min: 4}),
  (req, res) => {
-  console.log(`* service3097 called, req.originalUrl=${req.originalUrl}`);
-
   const errors = validationResult(req);
+  console.log(query.isEmpty)
+  console.log(errors)
   if (!errors.isEmpty()) {    
-    console.log("Validation error!!");
-    res.status(400).send(  
-      `<form method="GET" action="http://178.172.195.18:8180/service3097">
-      <label for="">Login</label>
-      <input type="text" name="login" value="${req.query.login || ''}">  
-      <label for="">Password</label>
-      <input type="password" name="password" value="${req.query.password || ''}">  
-      <input type="submit" value="SEND">
-      </form>
-      <p for="" style="color:red">Validation error!!</p>
-      `
-    );
+    res.status(400).send(initForm(req.query.login, req.query.password) + errorMsg);
   }
   else {
-    console.log(`Login: ${req.query.login}, Password: ${req.query.password}`);
-    res.status(200).send(`<p style="color:green">Login: ${req.query.login}, Password: ${req.query.password}</p>`);
+    if(!req.query.login || !req.query.password)
+      res.status(200).send(initForm());
+    else 
+      res.status(200).send(`<p style="color:green">Login: ${req.query.login}, Password: ${req.query.password}</p>`);
   }
 });
 
