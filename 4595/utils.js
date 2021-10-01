7bf702db6c708a671a38986e0d1fed0a8266aa12
LV.Khomyakova@gmail.com <LV.Khomyakova@gmail.com>
2021-10-01 23:40:21 +0300

    add service4595

---
+++ 4595/utils.js
@@ -0,0 +1,20 @@
+const fsp = require('fs').promises;
+const os = require('os');
+
+function log(filePath, message) {
+  return new Promise((resolve, reject) => {
+    const date = new Date();
+    const time = date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
+
+    const logLine = `${time} : ${message}`;
+    console.log(logLine);
+
+    fsp.appendFile(filePath, logLine + os.EOL)
+    .then(() => resolve())
+    .catch((err) => reject(err));
+  });
+}
+
+module.exports = {
+  log,
+};
