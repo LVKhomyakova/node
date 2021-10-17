fdc1e2b7cfa1d0b3b57817bfc4a932f9b68a06a0
LV.Khomyakova@gmail.com <LV.Khomyakova@gmail.com>
2021-10-17 20:32:25 +0300

    update 5695

--- 5695/public/script.js
+++ 5695/public/script.js
@@ -1,4 +1,5 @@
 const API = 'http://178.172.195.18:8180';
+const API_WS = 'ws://178.172.195.18:8181';
 
 // --------------------- запросы к API -------------------------
 async function loadFilesList() {
@@ -25,9 +26,10 @@ async function getFile(fileName) {
     return "Ошибка запроса";
 }
 
-async function putFile(data) {
+async function putFile(data, connectionId) {
   const response = await fetch(API + `/upload`, {
     method: 'POST',
+    headers: {connectionid: connectionId},
     body: data
   });
   if(response.ok)     
@@ -36,21 +38,45 @@ async function putFile(data) {
     return "Ошибка запроса";
 }
 // --------------------------- RENDER ------------------------------
-
 function drawFilesList(data) {
-  console.log(data)
-  document.body.insertAdjacentHTML('beforeend', data);
+  const list =  document.getElementById('list');
+  list.innerHTML = '';
+  list.insertAdjacentHTML('afterbegin', data);
+}
+
+function showProgress(progress) {
+  document.getElementById('progress').innerText = progress + '%';
 }
 
 // --------------------------- *** ------------------------------
-async function upload() {
+async function upload() {  
+  const connectionId = Math.floor(Math.random() * 1000000);
   const file = document.getElementById('upload').files[0];
   const comment = document.getElementById('comment').value;
   const data = new FormData();
   data.append("file", file);
   data.append("comment", comment);
-  const res = await putFile(data);
-  console.log('file is uploaded')
+  
+  let connection = new WebSocket(API_WS); 
+  connection.onopen = () => connection.send(JSON.stringify({connectionId}))
+  connection.onmessage = (event) => showProgress(event.data)
+  connection.onerror = (error) => console.log('WebSocket error:', error);
+  connection.onclose = () => {
+    connection = null;
+    clearInterval(keepAliveTimer);
+  };
+  let keepAliveTimer = setInterval(() => {
+    connection.send('KEEP_ALIVE'); 
+  }, 5000); 
+  
+  try {
+    const res = await putFile(data, connectionId);
+    console.log('file is uploaded')
+    drawFilesList(await loadFilesList());  
+  } catch(err) {
+    showProgress(" Связь с ервером потеряна! Повторите попытку позже");
+    console.log(err);    
+  }
 }
 
 async function download(fileName) {
--- 5695/service5695.js
+++ 5695/service5695.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const WebSocket = require('ws');
 const path  = require('path');
 const cors = require('cors');
 const multer = require('multer');
@@ -8,11 +9,37 @@ const { log, getFile, getFilesList, writeCommentFile } = require('./utils');
 const LOG_FILE_NAME = path.join(__dirname, '_server.log');
 const webServer = express();
 const PORT = 8180;
+const PORT_WS = 8181;
 const CORS_OPTIONS = {
   origin: '*',
   headers: 'Content-Type',
   optionsSuccessStatus: 200
 }
+
+// ---------------------------------WebSocket connection--------------------
+const serverWS = new WebSocket.Server({ port: PORT_WS });
+let clients = [];
+serverWS.on('connection', (connection) => {
+  connection.on('message', (message) => {    
+    if (message.toString() === 'KEEP_ALIVE')
+      clients.find((client) => client.connection === connection).lastkeepalive = Date.now();
+    else 
+      clients.find((client) => client.connection === connection).connectionId = JSON.parse(message.toString()).connectionId;
+  });
+
+  clients.push({connection: connection, lastkeepalive: Date.now(), connectionId: null})
+});
+
+setInterval(()=>{
+  clients.forEach((client) => {
+    if ((Date.now() - client.lastkeepalive) > 5000) { 
+      client.connection.terminate();
+      client.connection = null;
+    }
+  });
+  clients = clients.filter((client) => client.connection);
+}, 1000);
+// ------------------------------------***---------------------------------------
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
@@ -26,13 +53,10 @@ const upload = multer({storage});
 
 webServer.set('view engine', 'handlebars');
 webServer.set('views', path.join(__dirname, 'views'));
-
-// webServer.use(express.json());
 webServer.use((req, res, next) => {
   log(LOG_FILE_NAME, '-request-');
   next();
 });
-
 webServer.use('/service5695', express.static(path.join(__dirname, 'public')));
 
 // ------------------------------------------------------ options
@@ -44,8 +68,9 @@ webServer.options('/upload', (req, res) => {
 });
 // ------------------------------------------------------ post
 
-webServer.post('/upload', upload.single('file'), (req, res) => {
+webServer.post('/upload', progress, upload.single('file'), (req, res) => {
   writeCommentFile(path.join(__dirname, 'DB', req.file.originalname + '.comment'), req.body.comment);
+  log(LOG_FILE_NAME, 'upload finished');
   res.status(200).send('Файл загружен');
 });
 
@@ -57,11 +82,19 @@ webServer.get('/', cors(CORS_OPTIONS), (req, res) => {
 
 
 webServer.get('/download', (req, res) => {  
-  // res.setHeader("Content-Disposition", `attachment; filename=${req.query.filename}`);
-  // res.setHeader("Content-Type", "application/octet-stream");
-  // res.send(getFile(req.query.filename));
   res.download(path.join(__dirname, 'DB', req.query.filename), req.query.filename);
 });
 
+// ------------------------------------------------------ middleware
+function progress(req, res, next) {
+  log(LOG_FILE_NAME, 'upload started');
+  const fileSize = req.headers['content-length'];
+  let progress = 0;
+  req.on('data', (chunk) => {
+    progress = (chunk.length * 100) / fileSize + progress;
+    clients.find((client) => client.connectionId === Number(req.headers.connectionid))?.connection?.send(Math.round(progress));
+  });
+ next();
+}
 
 webServer.listen(PORT, () => log(LOG_FILE_NAME, `---web server 5695: running on port ${PORT}---`))
